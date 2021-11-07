a3e89c2feeb6952bf96f3d51dcac0359b51c6a1d
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2021-11-07 17:48:39 +0100

    set up GlobalStyles

---
+++ src/style/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        /* font-family: "Open Sans"; */ // kommst von designtoken 
+       display: flex;
+       justify-content: center;
+       align-items: center;
+    
+    }
+`
+
+export default GlobalStyle
